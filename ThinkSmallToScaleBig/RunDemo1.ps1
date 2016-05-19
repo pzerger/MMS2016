65f4bcd796c213099953b71c34354c303cca09e6
Lee <leealanberg@gmail.com>
2016-05-19 07:38:56 -0500

    Updates

--- ThinkSmallToScaleBig/RunDemo1.ps1
+++ ThinkSmallToScaleBig/RunDemo1.ps1
@@ -1,11 +1,18 @@
 #Best "no bs" getting started blog:
 #https://blogs.msdn.microsoft.com/jcorioland/2016/04/28/getting-started-with-containers-and-docker-on-windows-server-2016-technical-preview-5/
 
+#Windows Core Docker
+docker run -it windowsservercore cmd
+
+#Pull IIS
 docker pull microsoft/iis:latest
 
+#Run!
+docker run -itd -p 8080:80 microsoft/iis cmd 
+
+#Inspect
 docker network inspect nat
 
-docker run -itd -p 8080:80 microsoft/iis cmd 
 
 docker ps
 
